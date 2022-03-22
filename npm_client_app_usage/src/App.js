98f3298a5266dd131201f89a826cd5885ad1bec4
Mohan <mohansai9009@gmail.com>
2022-03-22 10:20:31 +0530

    component usage project

---
+++ npm_client_app_usage/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import npmclientapps from 'npmclientapps';
+
+function App() {
+  return (
+    <div className="App">
+      <npmclientapps></npmclientapps>
+    </div>
+  );
+}
+
+export default App;
